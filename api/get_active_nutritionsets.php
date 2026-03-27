fe57e1f0d12a9311fd5b748e3c0abf073b567533
Rence <beedrill332@gmail.com>
2026-03-27 14:35:41 +0800

    test: changed get active nutrition set query to include userID

--- api/get_active_nutritionsets.php
+++ api/get_active_nutritionsets.php
@@ -1,11 +1,14 @@
 <?php
+session_start();
 require_once '../db.php';
 
 header('Content-Type: application/json');
 
 // Example: assuming you have a column like `isActive`
-$result = $conn->query("SELECT nutritionID FROM plantnutrionneed WHERE isActive = 1");
-
+$stmt = $conn->prepare("SELECT nutritionID FROM plantnutrionneed WHERE isActive = 1 AND userID = ?");
+$stmt->bind_param("i", $_SESSION['userID']);
+$stmt->execute();
+$result = $stmt->get_result();
 $active = [];
 
 while ($row = $result->fetch_assoc()) {
