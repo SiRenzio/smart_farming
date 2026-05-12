23c08a423bbb309333cf4f1ebb8f9ac61695c689
SiRenzio <beedrill332@gmail.com>
2026-05-12 08:44:47 +0800

    change path to db

--- api/check_if_offline.php
+++ api/check_if_offline.php
@@ -1,5 +1,5 @@
 <?php
-require_once 'db.php';
+require_once(__DIR__ . '/../db.php');
 session_start();
 header('Content-Type: application/json; charset=utf-8');
 date_default_timezone_set('Asia/Manila');
